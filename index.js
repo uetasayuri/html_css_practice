71fba36d07dcd2dc12d752607dc442f19562214a
uetasayuri <ueta.sayuri2018@gmail.com>
2022-03-31 21:55:34 +0900

    firstcommit

---
+++ index.js
@@ -0,0 +1,38 @@
+let greeting= "Hello World!" ;
+greeting="Hello World2!!";
+console.log(greeting);
+
+const bigGreeting="He..Hell..Hello World!";
+
+let inoki=[`いーち`,`にー`,`さーん`,`ダーー！！`]
+console.log(inoki[2]);
+
+let index = 0
+while(index < inoki.length){
+  console.log(inoki[index]);
+  index++;
+}
+
+const test=(arg) =>{
+if(inoki.length>5){
+  console.log(ボンバイエ);
+} else {
+  console.log("ボンバ・・！");
+}
+};
+
+const greeting2= {
+  color: "pink",
+  size: "large",
+  purfume: "mint"
+};
+
+window.alert("TEST!");
+
+document.getElementsByTagName("botton")[0].addEventListener("click", ()=>{
+  window.alert("Hello World!");
+})
+
+
+
+console.log(greeting2.size);
